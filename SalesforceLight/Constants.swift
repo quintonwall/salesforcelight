e849c44e3e75fc858b178f9c1ce5de9d69675555
Quinton Wall <quinton_wall@hotmail.com>
2019-07-07 05:55:05 -0700

    Update Constants.swift

--- SalesforceLight/Constants.swift
+++ SalesforceLight/Constants.swift
@@ -3,14 +3,4 @@
 //  SalesforceLight
 //
 //  Created by QUINTON WALL on 10/21/15.
-//  Copyright © 2015 QUINTON WALL. All rights reserved.
-//
-
-import Foundation
-
-
-let Salesforce =
-[
-    "consumerKey": "3MVG98XJQQAccJQdj_1as3MoZaRmAZnoiGYB3yw.GeEuod0jd9oAjqd0ZK92ZI8XwDwtPaPtNTJunQP5sykT.",
-    "consumerSecret": "3030266816879795215"
-]
+//  Copyright © 2015 QUINTON WALL
